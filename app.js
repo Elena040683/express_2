b24ac9fb1d9e5f42180dfeaa2c05fec365b8e75a
Elena <Elena3405674@mail.ru>
2024-11-04 00:07:49 +0300

    feature:res.render

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require("express");
 const products = require("./products");
 
 const app = express();
+app.set("json spaces", 8);
+
+app.get("/", (req, res)=> {
+  res.render("homepage", {title: "Home page"})
+});
 
 app.get("/products", (req, res) => {
   //res.send(products);
   res.json(products);
-  
+  // res.json(null);
+  // res.send(null);
 })
 
 
